5ce9d79d577770c6edb53a6252c9d199e793c4cb
Ahiwe09 <ahiwe12345@gmail.com>
2023-08-13 03:24:44 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,16 +3,20 @@
  * main - Entry point of the program
  *
  * Description: This program prints all single digit
- * numbers of base.
+ * numbers of base 10 starting from 0.
  *
  * Return: Always return 0
  */
 int main(void)
 {
-	int a;
+	int c;
 
-	for (a = 0; a < 10; a++)
-		printf("%d", a);
+	c = 10;
+	while
+		(c < 10) {
+			putchar(c + '0');
+		}
 	putchar('\n');
 	return (0);
 }
+
