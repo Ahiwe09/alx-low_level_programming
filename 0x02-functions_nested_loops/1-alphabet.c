538b6d40798c1aaf5fd60825e4fc9c98ec382bfd
Ahiwe09 <ahiwe12345@gmail.com>
2023-08-15 12:26:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: This function prints the alphabet,
+ *  in lowercase, followed by a new line.
+ *
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
