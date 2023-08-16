15f9b0ba20d1bd1db7e4c009c078b32825d6c49c
Ahiwe09 <ahiwe12345@gmail.com>
2023-08-16 11:37:55 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * custom_add - Adds two integers and returns the sum.
+ *
+ * @a: First integer to be added.
+ * @b: Second integer to be added.
+ *
+ * Return: The sum of a and b.
+ */
+int custom_add(int a, int b)
+{
+	return (a + b);
+}
+
