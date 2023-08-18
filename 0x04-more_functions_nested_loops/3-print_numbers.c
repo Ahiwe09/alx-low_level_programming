d63a441b9bde1a23f53b200bb3e7db7e57c43410
Ahiwe09 <ahiwe12345@gmail.com>
2023-08-18 11:37:12 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * the _putchar function to print each digit from 0 to 9,
- * followed by a new line character.
- *
+ * This is the-function that prints the numbers,
+ * from 0 to 9, followed by a new line.
  * Return: always 0.
  */
-void print_numbers(void);
+void print_numbers(void)
 {
 	int c;
 
