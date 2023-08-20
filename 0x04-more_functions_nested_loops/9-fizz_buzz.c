67c547049516814580177bad33716a6b88dec807
Ahiwe09 <ahiwe12345@gmail.com>
2023-08-20 10:02:27 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
  * main - function  program that prints the
  * numbers from 1 to 100, followed by a new line.
  * print Fizz instead of the number.
- * Declare a global variable
+ *
  * Return: Alwas 0.
  */
 int main(void)
@@ -17,7 +17,6 @@ int main(void)
 
 		else if ((num % 3) == 0)
 			printf("FIZZ");
-
 		else if ((num % 5) == 0)
 			printf("BUZZ");
 		else
