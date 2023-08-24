1943b61e0077267de6c572b640c7363797fcead6
Ahiwe09 <ahiwe12345@gmail.com>
2023-08-24 09:51:26 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,25 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int n = 0;
-	int c = 0;
+	int n;
+	int c;
 
+	n = 0;
 	while (dest[n] != '\0')
 	{
-		n++;
+		n = n + 1;
 	}
-
+	c = 0;
 	while (src[c] != '\0')
 	{
-		if (n < MAX_DEST_LENGTH - 1)
-		{
-			dest[n] = src[c];
-			n++;
-		}
-		c++;
+		dest[n] = src[c];
+		n = n + 1;
+		c = c + 1;
 	}
 
 	dest[n] = '\0';
-
 	return (dest);
 }
