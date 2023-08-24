f9ea5b43236ef61c9cb9f75f9b09f865a7a08f66
Ahiwe09 <ahiwe12345@gmail.com>
2023-08-24 12:08:08 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,14 +15,14 @@ char *_strncat(char *dest, char *src, int n)
 	a = 0;
 	while (dest[a] != '\0')
 	{
-		a = a + 1;
+		a++;
 	}
 	x = 0;
 	while (x < n && src[x] != '\0')
 	{
-	dest[x] = src[x];
-	a = a + 1;
-	x = x + 1;
+		dest[a] = src[x];
+		a++;
+		x++;
 	}
 	dest[a] = '\0';
 	return (dest);
