4cc1f6740d2f06242f5382618f2f7ee8acd62b0d
Ahiwe09 <ahiwe12345@gmail.com>
2023-08-29 11:59:54 -0700

    7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,11 +5,15 @@
  *
  * Return: 0.
  */
-void print_chessboard(char (*a)[8]) {
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			printf("%c ", a[i][j]);
-		}
-		printf("\n");
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+
+		_putchar(a[i][j]);
 	}
+	_putchar('\n');
 }
