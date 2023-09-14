874360ba87ee432907e1aa44c69dbf38ae52bf48
Ahiwe09 <ahiwe12345@gmail.com>
2023-09-14 03:35:24 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,15 +1,6 @@
-#include "function_pointers.h"
+#include "stdio.h"
 #include <stdlib.h>
 #include "3-calc.h"
-#include <stdio.h>
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-
 /**
  * op_add - Returns the sum of two numbers.
  * @a: The first number.
