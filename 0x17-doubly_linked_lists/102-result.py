5ab5ea4255c45c5b90583d8df63fb70e8199c9ec
Ahiwe09 <ahiwe12345@gmail.com>
2023-12-08 15:07:23 -0800

    commit

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -7,10 +7,11 @@ def is_palindrome(num):
 
     for i in range(100, 1000):
         for j in range(100, 1000):
-        product = i * j
+            product = i * j
         if is_palindrome(product) and product > largest_palindrome:
             largest_palindrome = product
 
     with open('102-result', 'w') as file:
-    file.write(str(largest_palindrome))
+
+        file.write(str(largest_palindrome))
 
